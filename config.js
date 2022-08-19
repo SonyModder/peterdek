da5b60dc0ee37073030ceb5434ef974f6c1add50
SonyModder <111001627+SonyModder@users.noreply.github.com>
2022-08-19 22:06:57 +0700

    Update config.js

--- config.js
+++ config.js
@@ -12,10 +12,10 @@ global.APIKeys = {
 }
 
 // NAMBAH BOLEH HAPUS JANGAN -_-
-global.owner = ['6281511480762']
-global.premium = ['621511480762']
+global.owner = ['6285220980276']
+global.premium = ['6285220980276']
 global.packname = 'AzzBot√'
-global.author = 'AzzBot-MD'
+global.author = 'PETER-MD'
 global.sessionName = 'Xavior'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
